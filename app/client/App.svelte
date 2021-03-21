253868937c8ff0d6a553e0aca715fc8c6855cef4
arggh <>
2021-03-21 13:33:54 +0200

    Init

---
+++ app/client/App.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { Box } from "ui";
+</script>
+
+<Box>
+  <h1>Hello</h1>
+  <p>
+    Please edit <code>/packages/ui/Box.svelte</code> and observe how the app reloads completely.
+  </p>
+</Box>
---
+++ packages/ui/Box.svelte
@@ -0,0 +1,14 @@
+<script>
+
+</script>
+
+<div class="box">
+  <slot></slot>
+</div>
+
+<style>
+  .box {
+    padding: 1rem;
+    background-color: #ccc; /* Change this, for example */
+  }
+</style>
